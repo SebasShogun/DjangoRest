2dc187093e33ce49ba1de7b4b549c273aab9711c
SebasShogun <shogun31sff@gmail.com>
2020-06-20 20:03:23 -0500

    Fin de carpeta 2, cambios en viewsets

--- listelement/viewsets.py
+++ listelement/viewsets.py
@@ -1,8 +1,7 @@
 from rest_framework import viewsets
 from rest_framework.response import Response
-
+from rest_framework.decorators import action
 from django.shortcuts import get_object_or_404
-
 from .models import Element, Category, Type
 from .serializer import ElementSerializer, CategorySerializer, TypeSerializer
 
@@ -13,6 +12,12 @@ class ElementViewSet(viewsets.ModelViewSet):
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+
+    @action(detail=True, methods=['get'])
+    def elements(self, request, pk=None):
+        queryset = Element.objects.filter(category_id=pk)
+        serializer = ElementSerializer(queryset, many=True)
+        return Response(serializer.data)
 
     '''
     def list(self, request):
@@ -31,3 +36,9 @@ class CategoryViewSet(viewsets.ModelViewSet):
 class TypeViewSet(viewsets.ModelViewSet):
     queryset = Type.objects.all()
     serializer_class = TypeSerializer
+
+    @action(detail=True, methods=['get'])
+    def elements(self, request, pk=None):
+        queryset = Element.objects.filter(type_id=pk)
+        serializer = ElementSerializer(queryset, many=True)
+        return Response(serializer.data)
